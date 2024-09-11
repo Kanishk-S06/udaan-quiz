3b4141b9dc16f7103b62148c79de26ce061822e9
Kanishk Sharma <121635906+Kanishk-S06@users.noreply.github.com>
2024-09-11 21:55:40 +0530

    Update page.tsx
    css changes 2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Chat from "@/components/chat"
 
 export default function Home() {
   return (
-    <main className=" bg-gray-300 flex min-h-screen flex-col items-center justify-between p-24">
+    <main className=" bg-black flex min-h-screen flex-col items-center justify-between p-24">
       <Chat/>
     </main>
   )
